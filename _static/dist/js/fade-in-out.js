6e3b704a5e868769a788e2f9776d3c7effe11d03
unknown <flaming.june123@gmail.com>
2022-09-09 07:20:40 +0900

    20220909_0720

--- _static/dist/js/fade-in-out.js
+++ _static/dist/js/fade-in-out.js
@@ -1,6 +1,6 @@
 jQuery(function($){
   $(window).on('scroll', function(){
-    if ($(window).scrollTop() > 100) {
+    if ($(window).scrollTop() > 70) {
       $('.p-header__title__wrapper').css('visibility','hidden');
     } else {
       $('.p-header__title__wrapper').css('visibility','');
