4fb69568c4dc2f499c838e6ccc5574f34106e73e
unknown <flaming.june123@gmail.com>
2022-09-11 21:19:51 +0900

    20220911_2119

--- _static/dist/js/index.js
+++ _static/dist/js/index.js
@@ -1,23 +1,6 @@
 /*ブレイクポイント時の処理*/
 const mediaQueryList = window.matchMedia("(max-width:768px)");
 
-/*イベントリスナー*/
-
-const listener = (event) => {
-    //メディアクエリの設定
-    if(event.matches){
-        //768px以下の時
-        $(".p-item-section1").wrapAll('<div class="p-item-section--container1">');
-        $(".p-item-section2").wrapAll('<div class="p-item-section--container2">');
-        $(".p-item-section3").wrapAll('<div class="p-item-section--container1">');
-    } else if($('.p-item-section--container1').length || $('.p-item-section--container2').length) {
-        //768px以上の時
-        $(".p-item-section1").unwrap();
-        $(".p-item-section2").unwrap();
-        $(".p-item-section3").unwrap();
-    }
-};
-
 
 
 // リスナー登録
--- _static/dist/js/on.click.js
+++ _static/dist/js/on.click.js
@@ -2,9 +2,12 @@
 $('.p-nav--btn').on('click', function () {
     $('.p-header--nav-body').toggleClass('open');
     $('.p-nav--btn > a').toggleClass('open');
+    $('.p-header__nav--sp').toggleClass('active');
 });
 
-/*サブメニューのクリックイベント*/
+
+/*サブメニューのクリックイベント
 $('.p-nav--service').on('click', function () {
     $('.p-sub-menu--service').toggleClass('open');
 });
+*/
