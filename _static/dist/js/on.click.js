203fbe7a7ef5e452aeb2cca3fda4731ef846c9e5
naruiyamazaki <flaming.june123@gmail.com>
2022-09-12 17:29:38 +0900

    20220912_1729

--- _static/dist/js/on.click.js
+++ _static/dist/js/on.click.js
@@ -1,6 +1,5 @@
 /*ハンバーガーメニューのクリックイベント*/
 $('.p-nav--btn').on('click', function () {
-    $('.p-header--nav-body').toggleClass('open');
     $('.p-nav--btn > a').toggleClass('open');
     $('.p-header__nav--sp').toggleClass('active');
 });
